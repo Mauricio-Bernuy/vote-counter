546df125691e3fde1eace4cb9b8499638cfcada8
Mauricio-Bernuy <mauricio.bernuy@utec.edu.pe>
2023-11-12 10:30:58 -0500

    listed more commands, will now repull images correctly for development purposes

--- kube-test/app_kube.py
+++ kube-test/app_kube.py
@@ -1,9 +1,52 @@
-from flask import Flask
+# from flask import Flask
+# app = Flask(__name__)
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+
+from flask import Flask, request, jsonify
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "Hello, KUBE 2!"
 
-if __name__ == "__main__":
+@app.route('/upload-excel', methods=['POST'])
+def upload_excel():
+    try:
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+
+        file = request.files['file']
+
+        # If the user does not select a file, the browser may send an empty file without a filename
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        # Check if the file is allowed based on the file extension
+        if file and file.filename.endswith('.xlsx'):
+            # Save the file to a temporary location
+            file_path = 'uploads/' + file.filename
+            file.save(file_path)
+
+            # Read the Excel file using pandas
+            df = pd.read_excel(file_path)
+
+            # You can now use the dataframe 'df' for further processing
+
+            # Return the data as JSON
+            return jsonify({'success': True, 'data': df.to_dict(orient='records')}), 200
+
+        else:
+            return jsonify({'error': 'Invalid file format. Please upload a valid Excel file (.xlsx)'}), 400
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    # app.run(debug=True)
     app.run(host="0.0.0.0", port=5000)
--- kube-test/cmds.md
+++ kube-test/cmds.md
@@ -1,19 +1,28 @@
 ## build docker image
-docker build -t test-image .
+eval $(minikube docker-env) && docker build -t test-image:local .
 
 ## setup kubernetes cluster
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
-minikube service test-dep
+minikube service test-dep // creates ip in order to connect from localhost
 
-## delete cluster (in case you change it)
+## rebuild image and update cluster pods
+eval $(minikube docker-env) && docker build -t test-image:local .
+kubectl rollout restart -n default deployment test-dep
+
+## delete cluster (if needed)
 kubectl delete -f deployment.yaml
 kubectl delete -f service.yaml
+
+## open kubernetes dashboard
+minikube dashboard
 
 ## list online systems
 kubectl get deployments
 kubectl get pods
 kubectl get services
+
+
 
 ## other commands
 kubectl rollout status deployment/test-dep
